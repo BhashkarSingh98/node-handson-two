9aa311bfc98e4fc13cb51e718d366556c3094749
bhashkar <bhashkar2600@gmail.com>
2022-10-14 18:57:37 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,9 @@
+const data=[
+    {name:'bhashkar singh',email:'bhashkar12@gmail.com',contect:'1234567890'},
+    {name:'kamlesh singh',email:'kamlesh12@gmail.com',contect:'1234567890'},
+    {name:'deepak singh',email:'deepak12@gmail.com',contect:'1234567890'},
+    {name:'vijay singh',email:'vijay12@gmail.com',contect:'1234567890'}
+
+]
+
+module.exports=data;
